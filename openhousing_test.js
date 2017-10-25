8673f791ce93790c60a826f198884a24d2f1dbf5
housing <rohit.k@housing.com>
2017-10-25 14:46:19 +0530

    rentservice

--- openhousing_test.js
+++ openhousing_test.js
@@ -9,12 +9,16 @@ var driver = new webdriver.Builder()
 driver.get("https://www.housing.com");
 console.log('launched housing');
 
+/*Select Rent service*/
+driver.findElement(webdriver.By.className("tag inline rent")).click();
+console.log("rent");
+
 /*select a city
 driver.findElement(webdriver.By.xpath(".//*[@id='header']/header/div[1]/div[2]/div/div/span/a[3]/span/span[1]")).click();*/
 driver.findElement(webdriver.By.className("city-select-image mumbai")).click();
 console.log('city Mum');
 
-/*click on searchbar
+/*click on searchbox
 driver.findElement(webdriver.By.xpath(".//*[@id='home-page']/div/div[2]/div[1]/div/div/div/div[1]/div/span")).click();
 console.log('searchbar');*/
 driver.findElement(webdriver.By.className("last-search-wrapper")).click();
